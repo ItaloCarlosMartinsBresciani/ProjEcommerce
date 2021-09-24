771af82b61cdefd374b96e41bf787776908f5fca
Italo <86802934+ItaloCarlosMartinsBresciani@users.noreply.github.com>
2021-09-24 11:28:51 -0300

    Add files via upload

---
+++ ProjEcommerce-main/cadastro2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+
+<head>
+  <meta charset="UTF-8">
+  <link rel="icon" href="imagem/icon.png">
+  <title>Login</title>
+  <link rel="stylesheet" type="text/css" href="css/form.css" />
+  <script src="https://kit.fontawesome.com/8545ffdfda.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+  
+
+</body>
+</html>
