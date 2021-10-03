e30277f74866b8c6286105f90bcc60bc4bb43e20
Italo <86802934+ItaloCarlosMartinsBresciani@users.noreply.github.com>
2021-10-03 19:04:23 -0300

    Add files via upload

---
+++ ProjEcommerce-main/login.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+
+<head>
+  <meta charset="UTF-8">
+  <link rel="icon" href="imagem/icon.png">
+  <title>Login</title>
+  
+  <link rel="stylesheet" type="text/css" href="css/form.css" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/8545ffdfda.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+<?php
+        session_start();
+        if (!isset($_SESSION["logou"]))
+        {
+          echo "<div id='login-container'>
+          <h1><i class='fas fa-user-plus'></i> Login</h1>
+          <form action='php/cadastro.php' method='POST'>
+          
+            <label for='email'>E-mail</label>
+            <input type='email' name='login' id='login' placeholder='Digite seu e-email'>
+      
+            <label for='password'>Senha</label>
+            <input type='password' name='senha' id='senha' placeholder='Digite sua senha'>
+          
+            
+            <p>Ainda não tem uma conta? <a href='cadastro2.html'>Cadastre-se...</a></p>
+            
+            <input type='submit' value='Login'>
+          </form>
+          <form action='index.php' method='POST'>
+          <input type='submit' value='Voltar'>
+          </form>
+        </div>";
+        }
+       else{
+        //include "conexao.php";
+        //$sql="SELECT * FROM usuario WHERE excluido ='false' and  ORDER BY idusuario";
+        //$resultado= pg_query($conecta, $sql);
+        //$qtde=pg_num_rows($resultado);
+
+
+        echo " 
+        <link rel='stylesheet' type='text/css' href='css/estilos.css' />
+        <center>
+        <div id='mae2'>
+    
+          <br>
+          <a name='topo'></a>
+
+          
+                    
+              
+                
+          <div id='camada1'>
+            <br>
+            <a href='index.php'><img id='user' src='imagem/user.png'></a>
+            <br><br>";
+            
+                    
+                    include "php/adm.php";
+                    $adm = isMaster();
+                    if($adm){
+                        echo "<a href='tabelas_usu.php'><i class='fas fa-cogs'></i></a>";
+                         }
+                echo "  
+            <a class='um' href='index.php'>Home <i class='fas fa-home'></i></a>&nbsp;&nbsp;
+            <a class='um' href='produtos.php'>Produtos <i class='fas fa-desktop'></i></a>&nbsp;&nbsp;
+            <a class='um' href='.html'>Comprar <i class='fas fa-shopping-cart'></i></a>&nbsp;&nbsp;
+            <a class='um' href='devs.php'>Desenvolvedores <i class='fab fa-dev'></i></a>&nbsp;&nbsp;
+            <a class='um' href='.html'>Estatísticas</a>&nbsp;&nbsp;
+            <a class='um' href='login.php'>Info <i class='fas fa-info'></i></a>&nbsp;&nbsp;
+            <br><br><br>
+          </div>
+          
+        
+        <br><br><br>
+
+        <section>
+        <h1><i class='fas fa-info-circle'></i> Informações da conta</h1><br> <br> 
+        <center>
+        <i class='far fa-user'></i> Nome:       <br><br>        
+        <i class='fas fa-envelope-open-text'></i> Email:   <br><br>  
+        <i class='fas fa-database'></i> CPF:                 <br> <br> 
+        <i class='fas fa-map-marker-alt'></i> CEP:        <br> <br> 
+        <i class='fas fa-mobile'></i> Telefone:                <br> <br> 
+
+        </center>
+        <fieldset>
+        <label> 
+        
+        </label>
+        </fieldset>
+        <form action='php/logoff.php' method='POST'>
+      
+          
+          <input type='submit' value='Sair'>
+      
+        </section>
+        <br><br><br>
+        <div id='camada3'>
+        <br>
+        <a class='dois' href=index.php'>Home</a>&nbsp;&nbsp;
+        <a class='dois' href='produtos.php'>Produtos</a>&nbsp;&nbsp;
+        <a class='dois' href='.html'>Comprar</a>&nbsp;&nbsp;
+        <a class='dois' href='devs.php'>Desenvolvedores</a>&nbsp;&nbsp;
+        <a class='dois' href='.html'>Estatísticas</a>&nbsp;&nbsp;
+        <a class='dois' href='login.php'>Info</a>&nbsp;&nbsp;
+        <br><br>
+        <a class='tres' href='#topo'>Clique para voltar ao topo</a>
+        <br> <br>
+    </div>
+    <br><br>
+    05 - Ellen Lorenz Vieira Antonetti 13 - Italo C. Martins Bresciani 18 - Laura Lima Denardi 26 - Nathan
+    Braian Mariano Anunciação 34 - Ulisses Rodrigues Barreto
+    <br>
+ 
+    </div>
+  </center>
+
+
+        ";
+       }
+      ?>
+  
+
+
+
+
+
+
+
+
+
+</body>
+</html>
---
+++ ProjEcommerce-main/produtos.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+
+<head>
+  <meta charset="UTF-8">
+  <link rel="icon" href="imagem/icon.png">
+  <title>Produtos</title>
+  <link rel="stylesheet" type="text/css" href="css/estilos.css" />
+  <script src="https://kit.fontawesome.com/8545ffdfda.js" crossorigin="anonymous"></script>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+  <center>
+    <div id="mae">
+
+      <br>
+      <a name="topo"></a>
+      
+      <div id="camada1">
+        <br>
+
+        <a href="index.html"><img id="user" src="imagem/user.png"></a>
+        <br><br>
+        <?php 
+                    session_start();
+                    include "php/adm.php";
+                    $adm = isMaster();
+                    if($adm){
+                        echo "<a href='tabelas_prod.php'><i class='fas fa-cogs'></i></a>";
+                         }
+                ?>
+        <a class="um" href="index.php">Home <i class="fas fa-home"></i></a>&nbsp;&nbsp;
+        <a class="um" href="produtos.php">Produtos <i class="fas fa-desktop"></i></a>&nbsp;&nbsp;
+        <a class="um" href=".html">Comprar <i class="fas fa-shopping-cart"></i></a>&nbsp;&nbsp;
+        <a class="um" href="devs.php">Desenvolvedores <i class="fab fa-dev"></i></a>&nbsp;&nbsp;
+        <a class="um" href=".html">Estatísticas</a>&nbsp;&nbsp;
+        <?php
+        
+        if (!isset($_SESSION["logou"]))
+        {
+            echo "<a class='um' href='login.php'>Login <i class='fas fa-sign-in-alt'></i></a>&nbsp;&nbsp;
+            <br><br><br>";
+        }
+        else{
+            echo "<a class='um' href='login.php'>Info <i class='fas fa-info'></i></a>&nbsp;&nbsp;
+            <br><br><br>";
+        }
+        ?>
+
+        
+        <br><br><br>
+
+      </div>
+
+      <br><br><br><br>
+
+      <div id="camada_especial3">
+        <div id="texto_produto1">
+          <img src="imagem/historia.jpeg" width="150px" height="120px">
+        </div>
+        <div id="texto_produto2">
+          <br>
+          <h4>
+            Curso de historia falando sobre o feminismo, seus antecedentes, consequencias e seus
+            procedentes.
+          </h4>
+        </div>
+
+        
+
+        <div id="texto_produto1">
+          <img src="imagem/culinaria.jpg" width="150px" height="120px">
+        </div>
+        <div id="texto_produto2">
+          <br><br>
+          <h4>
+            Curso de culinaria ensinando uma receita facil de brownie de um jito fácil, rápido e saudável.
+          </h4>
+        </div>
+
+        <br><br>
+
+        <div id="texto_produto1">
+          <img src="imagem/origami.jpg" width="150px" height="120px">
+        </div>
+        <div id="texto_produto2">
+          <br>
+          <h4>
+            Curso de dobraduras ensinando a fazer os tres origamis mais faceis e simples para quem está comecando.
+          </h4>
+        </div>
+
+        
+
+        <div id="texto_produto1">
+          <img src="imagem/game.jpeg" width="150px" height="120px">
+        </div>
+        <div id="texto_produto2">
+          <br><br>
+          <h4>
+            Curso de videogame ensinando iniciantes a jogarem Minecraft.
+          </h4>
+        </div>
+
+        <br><br>
+
+        <div id="texto_produto1">
+          <img src="imagem/prog1.jpeg" width="150px" height="120px">
+        </div>
+        <div id="texto_produto2">
+          <br>
+          <h4>
+            Primeira parte de um curso sobre a linguagem de programação C++ para iniciantes que é dividido em duas
+            partes.
+          </h4>
+        </div>
+
+
+        <div id="texto_produto1">
+          <img src="imagem/prog2.jpeg" width="150px" height="120px">
+        </div>
+        <div id="texto_produto2">
+          <br><br>
+          <h4>
+            Segunda e ultima parte de um curso sobre a linguagem de programação C++ para iniciantes.
+          </h4>
+        </div>
+
+        <br><br>
+      </div>
+
+      <br><br><br><br><br>
+
+
+      
+      <div id="camada3">
+        <br>
+        <a class="dois" href="index.php">Home</a>&nbsp;&nbsp;
+        <a class="dois" href="produtos.php">Produtos</a>&nbsp;&nbsp;
+        <a class="dois" href=".html">Comprar</a>&nbsp;&nbsp;
+        <a class="dois" href="devs.php">Desenvolvedores</a>&nbsp;&nbsp;
+        <a class="dois" href=".html">Estatísticas</a>&nbsp;&nbsp;
+        <?php
+                    
+                    if (!isset($_SESSION["logou"]))
+                    {
+                        echo "<a class='dois' href='login.php'>Login</a>&nbsp;&nbsp;
+                        <br><br>";
+                    }
+                    else{
+                      echo "<a class='dois' href='login.php'>Info </a>&nbsp;&nbsp;
+                        <br><br>";
+                    }
+                    ?>
+        
+        <a class="tres" href="#topo">Clique para voltar ao topo</a>
+        <br> <br>
+    </div>
+    <br><br>
+    05 - Ellen Lorenz Vieira Antonetti 13 - Italo C. Martins Bresciani 18 - Laura Lima Denardi 26 - Nathan
+    Braian Mariano Anunciação 34 - Ulisses Rodrigues Barreto
+    <br>
+    </div>
+    </div>
+
+
+  </center>
+
+
+
+</body>
+
+</html>
